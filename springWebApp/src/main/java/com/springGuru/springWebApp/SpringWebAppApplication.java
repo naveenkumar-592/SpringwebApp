910dbf19d697c19f2a8527df0e2a2ce0d028457b
naveenkumar-592 <naveenkumar1355@gmail.com>
2019-10-10 23:08:18 +0530

    Commit First Proj

---
+++ springWebApp/src/main/java/com/springGuru/springWebApp/SpringWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.springGuru.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Application to run the 
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
